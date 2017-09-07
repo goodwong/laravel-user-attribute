36caa6f0f093a85e620b1bb70c1e9e9152ba87b0
william <goodwong@foxmail.com>
2017-09-07 18:55:57 +0800

    user-values index action

--- src/Http/Controllers/UserValueController.php
+++ src/Http/Controllers/UserValueController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Goodwong\LaravelUserAttribute\Http\Controllers;
 
-use App\Goodwong\LaravelUserAttribute\Entities\UserValue;
+use Goodwong\LaravelUserAttribute\Entities\UserValue;
+use Goodwong\LaravelUserAttribute\Handlers\UserDataHandler;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class UserValueController extends Controller
@@ -10,11 +12,41 @@ class UserValueController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // 搜索
+        $keyword = $request->input('search');
+        $context = $request->input('context');
+        if ($keyword && $context) {
+            $handler = new UserDataHandler($context);
+            return $handler->search($keyword);
+        }
+        // 多个用户数据（数据矩阵）
+        $user_ids = $request->input('users');
+        $attribute_ids = $request->input('attributes');
+        if ($user_ids && $attribute_ids) {
+            $handler = new UserDataHandler();
+            return $handler->valuesOfMany(explode(',', $user_ids), explode(',', $attribute_ids));
+        }
+        // 单个用户数据
+        $user_id = $request->input('user');
+        $attribute_ids = $request->input('attributes');
+        if ($user_id && $attribute_ids) {
+            $handler = new UserDataHandler();
+            return $handler->values($user_id, explode(',', $attribute_ids));
+        }
+        // 数据历史
+        $user_id = $request->input('user');
+        $attribute_id = $request->input('attribute');
+        if ($user_id && $attribute_id) {
+            $handler = new UserDataHandler();
+            return $handler->history($user_id, $attribute_id);
+        }
+        // nothing
+        return collect();
     }
 
     /**
@@ -41,7 +73,7 @@ class UserValueController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Goodwong\LaravelUserAttribute\Entities\UserValue  $userValue
+     * @param  \Goodwong\LaravelUserAttribute\Entities\UserValue  $userValue
      * @return \Illuminate\Http\Response
      */
     public function show(UserValue $userValue)
@@ -52,7 +84,7 @@ class UserValueController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Goodwong\LaravelUserAttribute\Entities\UserValue  $userValue
+     * @param  \Goodwong\LaravelUserAttribute\Entities\UserValue  $userValue
      * @return \Illuminate\Http\Response
      */
     public function edit(UserValue $userValue)
@@ -64,7 +96,7 @@ class UserValueController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Goodwong\LaravelUserAttribute\Entities\UserValue  $userValue
+     * @param  \Goodwong\LaravelUserAttribute\Entities\UserValue  $userValue
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, UserValue $userValue)
@@ -75,7 +107,7 @@ class UserValueController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Goodwong\LaravelUserAttribute\Entities\UserValue  $userValue
+     * @param  \Goodwong\LaravelUserAttribute\Entities\UserValue  $userValue
      * @return \Illuminate\Http\Response
      */
     public function destroy(UserValue $userValue)
